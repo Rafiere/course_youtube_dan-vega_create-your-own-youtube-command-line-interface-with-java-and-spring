7d73aac27e92ae2e00b9c4adc55472780eff1e4e
Rafiere <rafapgbr@gmail.com>
2023-11-05 08:15:21 -0300

    feat: add initial project files

---
+++ src/main/java/dev/piccodev/youtubecli/YoutubeCliApplication.java
@@ -0,0 +1,13 @@
+package dev.piccodev.youtubecli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/piccodev/youtubecli/YoutubeCliApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.piccodev.youtubecli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
