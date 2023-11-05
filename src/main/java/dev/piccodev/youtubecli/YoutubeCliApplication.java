6be73d5a04320c76f73d8cce1f697542d0ac4fed
Rafiere <rafapgbr@gmail.com>
2023-11-05 15:00:02 -0300

    feat: add enable configuration properties

--- src/main/java/dev/piccodev/youtubecli/YoutubeCliApplication.java
+++ src/main/java/dev/piccodev/youtubecli/YoutubeCliApplication.java
@@ -1,9 +1,12 @@
 package dev.piccodev.youtubecli;
 
+import dev.piccodev.youtubecli.config.YouTubeConfigProps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(YouTubeConfigProps.class)
 public class YoutubeCliApplication {
 
 	public static void main(String[] args) {
